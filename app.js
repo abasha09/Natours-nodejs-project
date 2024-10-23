84bbdb1bdd12e929bfbec82d83f70f31b8d547aa
irakli abashidze <irakli.abashidzeee@gmail.com>
2024-10-24 03:25:43 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+// 1) MIDDLEWARES
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!!!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// 4) START SERVER
+
+module.exports = app;
