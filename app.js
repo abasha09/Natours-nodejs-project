df1a4d76259813baead26eb8adb35bdf45b06e60
irakli abashidze <irakli.abashidzeee@gmail.com>
2024-10-26 03:06:58 +0400

    ENV variables

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-// 1) MIDDLEWARES
-
 const app = express();
 
-app.use(morgan('dev'));
+// 1) MIDDLEWARES
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware!!!');
